5f42d4187ddef038335a7d7cffe45df9c21a1387
Seungha Kim <sh.kim.x928@gmail.com>
2025-10-27 12:14:19 +0900

    Automate Android WebView dev flow for Vite

---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,2 @@
+# Keep rules for release builds. WebView apps typically do not require special rules,
+# but the file is kept to allow user customization.
---
+++ android/app/src/main/java/com/ebview/android/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.ebview.android
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.addCallback
+import androidx.core.view.WindowCompat
+
+class MainActivity : ComponentActivity() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        WebView.setWebContentsDebuggingEnabled(true)
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.cacheMode = WebSettings.LOAD_NO_CACHE
+        settings.setAppCacheEnabled(false)
+        settings.allowFileAccess = false
+        settings.allowContentAccess = false
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                return false
+            }
+        }
+        webView.webChromeClient = WebChromeClient()
+
+        val targetUrl = BuildConfig.VITE_DEV_SERVER_URL
+        webView.loadUrl(targetUrl)
+
+        onBackPressedDispatcher.addCallback(this) {
+            if (webView.canGoBack()) {
+                webView.goBack()
+            } else {
+                finish()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        webView.onResume()
+    }
+
+    override fun onPause() {
+        webView.onPause()
+        super.onPause()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        webView.destroy()
+    }
+}
